807012112e8b3dcf02c132a7837001f4fde0a9d4
kikiisa <kikiisa89@gmail.com>
2023-12-08 02:55:53 +0800

    fix

--- manager_file (1).sql
+++ manager_file (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2023 at 03:56 AM
+-- Generation Time: Dec 07, 2023 at 07:55 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -34,6 +34,13 @@ CREATE TABLE `admin` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `name`, `password`) VALUES
+(1, 'jodi', 'Jodi Binolombangan', '8d9a90a08e1fd8de1a1efe902016d02e');
+
 -- --------------------------------------------------------
 
 --
@@ -54,7 +61,10 @@ CREATE TABLE `file_upload` (
 INSERT INTO `file_upload` (`id`, `nama_file`, `path_file`, `keterangan`) VALUES
 (17, 'Ngobrol Engineer \"Di Balik Teknologi CHAT GPT\"', '656fdc2980e2b_baner_yayasan.jpg', 'testing'),
 (18, 'dkfdjfkj', '656fdca7e00d4_biodata_ibu_teti.docx', 'kdfjdkfjk'),
-(19, 'Testtt', '656fdcf45b2f7_GCCITXME_Pweb1.pdf', 'jodi bgst');
+(19, 'Testtt', '656fdcf45b2f7_GCCITXME_Pweb1.pdf', 'jodi bgst'),
+(20, 'foto sexi', '6571f277ab222_Screenshot_28.png', 'salahudin sexi'),
+(21, '', '6571f2a29c72c_', ''),
+(22, 'Fiksi', '6572106e06e9c_aldi-proposal-print.pdf', 'sdjskdjskjk');
 
 --
 -- Indexes for dumped tables
@@ -80,13 +90,13 @@ ALTER TABLE `file_upload`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `file_upload`
 --
 ALTER TABLE `file_upload`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
